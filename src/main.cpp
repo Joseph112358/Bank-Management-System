743e8f739de8419b92a52e0dfcb037cc36e981fb
= <=>
2023-11-17 02:21:33 +0000

    Add main menu and put in separate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
+#include "userInterface.h"
 
+// Main menu
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  showMainMenu();
+  int input;
+  std:: cout << "Your input: ";
+  std::cin >> input;
+  system("cls"); // Clear the screen to keep the UI clean / readable
+  std::cout << "Your input: " + std::to_string(input) << std::endl;
   return 0;
 }
---
+++ src/userInterface.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+void showMainMenu(){
+  std::cout << "Hello there. Welcome to Bank of Joe. What would you like to do?" << std::endl;
+  std::cout << " __________________________________________" << std::endl;
+  std::cout << "| 1) Create an account                    |" << std::endl;
+  std::cout << "| 2) Deposit money                        |" << std::endl;
+  std::cout << "| 3) Withdraw money                       |" << std::endl;
+  std::cout << "|_________________________________________|" << std::endl;
+}
